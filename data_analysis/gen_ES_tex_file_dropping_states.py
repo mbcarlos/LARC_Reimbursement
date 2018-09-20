6cf869f3ffff9e507b59ccfca0f5d8e45a76f5f7
Marisa Carlos <mbcarlos96@gmail.com>
2018-09-20 15:13:14 -0400

    add LARC to dropping states PDF, copy original event studies from graphs/event_studies/date

--- data_analysis/gen_ES_tex_file_dropping_states.py
+++ data_analysis/gen_ES_tex_file_dropping_states.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+from shutil import copyfile
 
-#date = raw_input("What is the date used to the name the folder? ")
-date = "19 Sep 2018"
+date = raw_input("What is the date used to the name the folder? ")
+#date = "19 Sep 2018"
 mypath = "/Users/marisacarlos/Dropbox/Cornell/Research/Projects/LARC_Reimbursement/graphs/exploratory_graphs/"+date+"/"
+orig_event_study_path = "/Users/marisacarlos/Dropbox/Cornell/Research/Projects/LARC_Reimbursement/graphs/event_studies/"+date+"/"
 
 
-state_drop_list = ["CA","CO","DE","DC","GA","ID","IL","IN","IA","LA","MD","MT","NM","NY","OK","RI","SC","TX","WA","WY"]
+state_drop_list_birth_data = ["CA","CO","DE","DC","GA","ID","IL","IN","IA","LA","MD","MT","NM","NY","OK","RI","SC","TX","WA","WY"]
+state_drop_list_larc_data = ["CT","FL","ID","LA","MA","ME","OK","RI","VT","WV"]
 
 tex_file_lines =[ r'''\documentclass[11pt]{article}''',r'''\usepackage{geometry}''',r'''\geometry{letterpaper}''',r'''\usepackage{graphicx}''',
                  r'''\usepackage{float}''',r'''\pagestyle{empty}''',r'''\begin{document}''',r''' \begin{center}''']
@@ -15,17 +18,29 @@ suffixes = ["total","unmarried","teen","hsorless"]
 
 ldq_or_fdq = "ldq"
 
+if Path(r'''%slarc_utilization_quarterly_ES.pdf'''%(orig_event_study_path)).is_file():
+    copyfile(orig_event_study_path+"larc_utilization_quarterly_ES.pdf", mypath+"larc_utilization_quarterly_ES.pdf")
+    tex_file_lines.append(r'''\textbf{EVENT STUDY INCLUDING ALL STATES:}''')
+    tex_file_lines.append(r'''    \includegraphics[width=.8\textwidth]{larc_utilization_quarterly_ES.pdf}''')
+    tex_file_lines.append(r'''\newpage''')
+    tex_file_lines.append(r'''\textbf{EVENT STUDIES DROPPING INDIVIDUAL STATES:}''')
+    for state in state_drop_list_larc_data:
+        if Path(r'''%slarc_utilization_quarterly_ES_no%s.pdf'''%(mypath,state)).is_file():
+            tex_file_lines.append(r'''    \includegraphics[width=.8\textwidth]{larc_utilization_quarterly_ES_no%s.pdf}''' %(state))
+    
+tex_file_lines.append(r'''\newpage''')
 
 for prefix in prefixes:
     for suffix in suffixes:
-        if Path(r'''%s%s_%s_%s_C2_quarter_ES.pdf'''%(mypath,prefix,suffix,ldq_or_fdq)).is_file():
+        if Path(r'''%s%s_%s_%s_C2_quarter_ES.pdf'''%(orig_event_study_path,prefix,suffix,ldq_or_fdq)).is_file():
+            copyfile(orig_event_study_path+prefix+"_"+suffix+"_"+ldq_or_fdq+"_C2_quarter_ES.pdf",mypath+prefix+"_"+suffix+"_"+ldq_or_fdq+"_C2_quarter_ES.pdf")
             tex_file_lines.append(r'''\textbf{EVENT STUDY INCLUDING ALL STATES:}''')
-            tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{%s_%s_%s_C2_quarter_ES.pdf}''' %(prefix,suffix,ldq_or_fdq))
+            tex_file_lines.append(r'''    \includegraphics[width=.8\textwidth]{%s_%s_%s_C2_quarter_ES.pdf}''' %(prefix,suffix,ldq_or_fdq))
             tex_file_lines.append(r'''\newpage''')
             tex_file_lines.append(r'''\textbf{EVENT STUDIES DROPPING INDIVIDUAL STATES:}''')
-            for state in state_drop_list:
+            for state in state_drop_list_birth_data:
                 if Path(r'''%s%s_%s_%s_C2_quarter_ES_no%s.pdf'''%(mypath,prefix,suffix,ldq_or_fdq,state)).is_file():
-                    tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{%s_%s_%s_C2_quarter_ES_no%s.pdf}''' %(prefix,suffix,ldq_or_fdq,state))
+                    tex_file_lines.append(r'''    \includegraphics[width=.8\textwidth]{%s_%s_%s_C2_quarter_ES_no%s.pdf}''' %(prefix,suffix,ldq_or_fdq,state))
                     
             tex_file_lines.append(r'''\newpage''')
             
