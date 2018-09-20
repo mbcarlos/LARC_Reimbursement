af3e630e46998c3cb099ee7b47a42c158ec9edc2
Marisa Carlos <mbcarlos96@gmail.com>
2018-09-20 14:09:18 -0400

    get rid of monthly graphs, add LARC use graph

--- data_analysis/gen_event_study_tex_files.py
+++ data_analysis/gen_event_study_tex_files.py
@@ -12,6 +12,11 @@ tex_file_lines =[ r'''\documentclass[11pt]{article}''',r'''\usepackage{geometry}
 ldq_or_fdq = "ldq"
 
 
+if Path(r'''%slarc_utilization_quarterly_ES.pdf''' %(mypath)).is_file():
+    tex_file_lines.append(r'''\begin{figure}[H]''')
+    tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{larc_utilization_quarterly_ES.pdf}''' %())
+    tex_file_lines.append(r'''\end{figure}''')
+
 for prefix in prefixes:
     for suffix in suffixes:
         if Path(r'''%s%s_%s_%s_C2_quarter_ES.pdf'''%(mypath,prefix,suffix,ldq_or_fdq)).is_file() or Path(r'''%s%s_%s_%s_C1_quarter_ES.pdf'''%(mypath,prefix,suffix,ldq_or_fdq)).is_file():
@@ -30,23 +35,24 @@ for prefix in prefixes:
             else:
                 tex_file_lines.append(r'''no quarterly 1st child/unknown graph for %s %s''' %(prefix,suffix))
             tex_file_lines.append(r'''\end{figure}''')
-            
-        if Path(r'''%s%s_%s_C2_month_ES.pdf'''%(mypath,prefix,suffix)).is_file() or Path(r'''%s%s_%s_C1_month_ES.pdf'''%(mypath,prefix,suffix)).is_file():
-            tex_file_lines.append(r'''\begin{figure}[H]''')
-            if Path(r'''%s%s_%s_C2_month_ES.pdf'''%(mypath,prefix,suffix)).is_file():
-                tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{%s_%s_C2_month_ES.pdf}''' %(prefix,suffix))
-            else:
-                tex_file_lines.append(r'''no monthly 2nd child graph for %s_%s''' %(prefix,suffix))
-                tex_file_lines.append(r''' \\ ''')
-                tex_file_lines.append(r''' \\ ''')
-                tex_file_lines.append(r''' \\ ''')
-            if Path(r'''%s%s_%s_C1_month_ES.pdf'''%(mypath,prefix,suffix)).is_file():
-                tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{%s_%s_C1_month_ES.pdf}''' %(prefix,suffix))
-            else:
-                tex_file_lines.append(r'''no monthly 1st child/unknown graph for %s %s''' %(prefix,suffix))
-        
-            tex_file_lines.append(r'''\end{figure}''')
-            tex_file_lines.append(r'''\newpage''')
+            #tex_file_lines.append(r'''\newpage''')
+        # NOT DOING MONTHLY GRAPHS ANYMORE
+        # if Path(r'''%s%s_%s_C2_month_ES.pdf'''%(mypath,prefix,suffix)).is_file() or Path(r'''%s%s_%s_C1_month_ES.pdf'''%(mypath,prefix,suffix)).is_file():
+        #     tex_file_lines.append(r'''\begin{figure}[H]''')
+        #     if Path(r'''%s%s_%s_C2_month_ES.pdf'''%(mypath,prefix,suffix)).is_file():
+        #         tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{%s_%s_C2_month_ES.pdf}''' %(prefix,suffix))
+        #     else:
+        #         tex_file_lines.append(r'''no monthly 2nd child graph for %s_%s''' %(prefix,suffix))
+        #         tex_file_lines.append(r''' \\ ''')
+        #         tex_file_lines.append(r''' \\ ''')
+        #         tex_file_lines.append(r''' \\ ''')
+        #     if Path(r'''%s%s_%s_C1_month_ES.pdf'''%(mypath,prefix,suffix)).is_file():
+        #         tex_file_lines.append(r'''    \includegraphics[width=\textwidth]{%s_%s_C1_month_ES.pdf}''' %(prefix,suffix))
+        #     else:
+        #         tex_file_lines.append(r'''no monthly 1st child/unknown graph for %s %s''' %(prefix,suffix))
+        # 
+        #     tex_file_lines.append(r'''\end{figure}''')
+        #     tex_file_lines.append(r'''\newpage''')
     
             
 print("%sevent_study_graphs_%s.tex" %(mypath, date))
